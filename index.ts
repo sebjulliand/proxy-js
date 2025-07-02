56e261bd2112b250ca6500007098c8033c268596
Seb Julliand <sjulliand@arcadsoftware.com>
2025-07-02 14:43:09 +0200

    Handle client reset connection
    Signed-off-by: Seb Julliand <sjulliand@arcadsoftware.com>

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import morgan from "morgan";
+import net from "net";
 import os from "os";
 
 dotenv.config({ quiet: true });
@@ -31,23 +32,31 @@ const app = express();
 // Logging the requests
 app.use(morgan("dev"));
 
+let checkAuth = (auth?: string) => true;
 if (proxyUser) {
-  app.use('', (req, res, next) => {
-    if (req.headers.authorization) {
-      const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  checkAuth = (auth?: string) => {
+    if (auth) {
+      const b64auth = (auth || '').split(' ')[1] || ''
       const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
       if (login === proxyUser && password === proxyPassword) {
-        return next();
+        return true;
       }
       else {
         console.error(`❌ Access denied: invalid user (${login}) or password`);
       }
     }
     else {
-      console.error(`❌ Access denied: no basic auth header was received: ${req.headers.authorization || "no authorization header"}`);
+      console.error(`❌ Access denied: no basic auth header was received: ${auth || "no authorization header"}`);
     }
-
-    res.sendStatus(403);
+    return false;
+  }
+  app.use('', (req, res, next) => {
+    if (checkAuth(req.headers.authorization)) {
+      return next();
+    }
+    else {
+      res.sendStatus(403);
+    }
   });
 }
 
@@ -63,11 +72,48 @@ app.use(
   })
 );
 
-app.connect("/",(req) => console.log(req)
-  
-);
+app.connect("/", (req, res) => {
+  console.log("hello")
+  console.log(req)
+});
 
 // Starting our Proxy server
-app.listen(Number(proxyPort), () => {
-  console.log(`Starting Proxy at http://${os.hostname()}:${proxyPort}/proxy - redirects to ${proxyTarget}${proxyUser ? " (🔑 Requires authentication)" : ""}`);
+const server = app.listen(Number(proxyPort), () => {
+  console.log(`Starting Proxy at http://${os.hostname()}:${proxyPort} - redirects to ${proxyTarget}${proxyUser ? " (🔑 Requires authentication)" : ""}`);
+});
+
+server.on("connect", (req, clientSocket, head) => {
+  if (req.url) {
+    console.log(`🔌 CONNECT request on ${req.url}`);
+    const authHeader = req.headers["proxy-authorization"];
+    if (!authHeader || checkAuth(authHeader)) {
+      const [host, port] = req.url.split(":");
+      if (host && port) {
+        const serverSocket = net.connect(port ? Number(port) : 443, host, () => {
+          console.log(`🔁 HTTPS tunnel established on ${host}:${port}`);
+          clientSocket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
+          serverSocket.write(head);
+          serverSocket.pipe(clientSocket);
+          clientSocket.pipe(serverSocket);
+        });
+        serverSocket.on("error", (err) => {
+          console.error(`❌ HTTPS tunneling error :`, err.message);
+          clientSocket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+          clientSocket.destroy();
+        });
+        clientSocket.on("error", (err: any) => {
+          if (err.code === "ECONNRESET") {
+            console.log("Connection reset by client");
+          }
+          else {
+            throw err;
+          }
+        })
+      }
+    }
+    else {
+      clientSocket.write("HTTP/1.1 403 Forbidden\r\n\r\n");
+      clientSocket.destroy();
+    }
+  }
 });
